37c40d56500ccba7afee79122c3327282fbb2ee1
Bimal Varghese <bimal.varghese@ADRAPIDVALUE.COM>
2017-04-01 12:34:46 +0530

    Implemented fetcing data from the server Using .get() method

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -7,7 +7,13 @@ angular.module('confusionApp')
         $scope.filtText = "";
 
         $scope.showDetails = false;
-        $scope.dishes= menuFactory.getDishes();
+        $scope.dishes= {};
+        menuFactory.getDishes()
+        .then(
+            function(response){
+                $scope.dishes=response.data;
+            }
+        )
 
 
         $scope.select = function (setTab) {
@@ -32,13 +38,26 @@ angular.module('confusionApp')
     }])
    .controller('DishDetailController', ['$scope','$stateParams', 'menuFactory',
              function($scope,$stateParams, menuFactory) {
-   var dish=menuFactory.getDish(parseInt($stateParams.id,10));
-   $scope.dish=dish;
+        $scope.dish={};
+        menuFactory.getDish(parseInt($stateParams.id,10))
+        .then(
+            function(response){
+                $scope.dish=response.data;
+                $scope.showDish=true;
+            }
+        );
 
 }])
-.controller('IndexController', ['$scope','$stateParams', 'menuFactory',
+.controller('IndexController', ['$scope','menuFactory',
              function($scope,$stateParams, menuFactory) {
-   var dish=menuFactory.getDish(parseInt($stateParams.id,10));
+                 $scope.dish={};
+                 menuFactory.getDish(0)
+                 .then(
+                     function(response){
+                         $scope.dish=response.data;
+                         $scope.showDish=true;
+                     }
+                 );
    $scope.dish=dish;
 
 }])
